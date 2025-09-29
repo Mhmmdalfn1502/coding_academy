8c54589c83fc337d765be8b91b4ac974a503148e
NagasaAnandes <nagasa.anandes@gmail.com>
2025-09-29 12:09:29 +0700

    Updated Base Project

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React from "react";
+import React from "react";
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
@@ -29,7 +30,5 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
-}
-
-export default App
+  );
+};
