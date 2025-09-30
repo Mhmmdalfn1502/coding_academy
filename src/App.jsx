3f31a6db09b03b64f390aafbc45764732ffa0fe1
NagasaAnandes <nagasa.anandes@gmail.com>
2025-09-30 17:29:31 +0700

    Updated Base #3

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import Home from "./pages/Home";
+import WebDevLearning from "./pages/WebDevLearning";
+import WebConsultant from "./pages/WebConsultant";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
-    <div>
-      <Home />
-    </div>);
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/program/webdevlearning" element={<WebDevLearning />} />
+        <Route path="/program/webconsultant" element={<WebConsultant />} />
+      </Routes>
+    </Router>);
 };
 
 export default App;
---
+++ src/pages/WebDevLearning.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WebDevLearning = () => {
+    return (
+        <div>
+            <h1>Web Developer Learning Page</h1>
+        </div>
+    );
+};
+
+export default WebDevLearning;
